3bbfe3828e89aa693362f0e07ce1de1273af5f13
Kaja Więckowska <k.wieckowska66@gmail.com>
2023-02-01 22:58:23 +0100

    Added identity tables

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -34,6 +34,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
